9b9f0c025fed886706fbd82348e3ae385af82c6b
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-06-26 13:44:22 -0400

    Logging model's location property in application.hbs

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,11 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(){
-    return new Ember.RSVP.Promise(function(resolve, reject) {
-      navigator.geolocation.getCurrentPosition(function(position){
-        resolve(position);
-        console.log(position);
-      });
+    return Ember.RSVP.hash({
+
+      location: new Ember.RSVP.Promise(function(resolve, reject) {
+        navigator.geolocation.getCurrentPosition(function(position){
+          resolve({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+          });
+          // console.log(position.coords.latitude, position.coords.longitude);
+        });
+      })
     });
-  }
+  },
 });
