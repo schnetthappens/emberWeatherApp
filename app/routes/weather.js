d1803d862dfcb9836b657114d44e5443e1c5b257
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-06-27 13:37:34 -0400

    Pass correct data object to weather template

--- app/routes/weather.js
+++ app/routes/weather.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   model: function(params){
     return Ember.$.ajax('https://api.forecast.io/forecast/01ad39e0ef141c8e3e01e954e0992d90/'+params.lat+','+params.lng,
      {dataType: 'jsonp'}).then(function(response){
-       console.log(response);
+       return(response);
     });
   },
 });
