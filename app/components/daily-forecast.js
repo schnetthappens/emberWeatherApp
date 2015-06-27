9f59613503f39867f00f0bd7e7c05bba2402c2ab
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-06-27 17:42:12 -0400

    Implement SlideToggle on daily forecast

--- app/components/daily-forecast.js
+++ app/components/daily-forecast.js
@@ -2,5 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'section',
-  classNames: ['weather-forecast']
+  classNames: ['weather-forecast'],
+
+  init: function(){
+      this._super();
+    },
+
+    actions: {
+      toggleDailyWeather: function(){
+        console.log('hello');
+        $('.weather-forecast-daily').slideToggle('slow', function(){});
+      }
+    }
+
 });
+// $(this.el).closest('.menu-category').find('.category-list').slideToggle('slow', function(){});
+// }
--- app/routes/weather.js
+++ app/routes/weather.js
@@ -5,8 +5,13 @@ export default Ember.Route.extend({
     return Ember.$.ajax('https://api.forecast.io/forecast/01ad39e0ef141c8e3e01e954e0992d90/'+params.lat+','+params.lng,
      {dataType: 'jsonp'}).then(function(response){
        return(response);
-    });
-  },
-});
+      });
+  }
 
-// +params.lat+','+params.lng,
+  // actions: {
+  //   toggleDailyWeather: function(){
+  //     console.log('hello');
+  //   }
+  // }
+
+});
