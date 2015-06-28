1914fd20b737b2519e9e8ee472f68eafdf30910b
John Schnettgoecke <john.schnettgoecke@gmail.com>
2015-06-28 10:54:55 -0400

    Implement sliceTemp to display current temp

--- app/components/weather-main.js
+++ app/components/weather-main.js
@@ -2,5 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'section',
-  classNames: ['weather-main']
+  classNames: ['weather-main'],
+
+  init: function(){
+    this.sliceTemp();
+    this._super();
+  },
+
+  sliceTemp: function(){
+    var temp = this.get('current.temperature');
+    temp = temp.toString().slice(0,2);
+    this.set('current.temperature', temp);
+  }
+
 });
